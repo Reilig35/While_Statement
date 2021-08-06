ece58a18d9c77ea891a1275cba9343998aa45abf
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-06 15:01:16 +0100

    Add project files.

---
+++ While_Statement/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+namespace While_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number between 1 and 5");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            bool Guess = false;
+
+            do 
+            { 
+                switch(num) 
+                {
+                    case 1:
+                        Console.WriteLine("Sorry it's not 1");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Correct it was 2 all along");
+                        Guess = true;
+                        break;
+                    case 3:
+                        Console.WriteLine("Sorry it's not 3");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("Sorry it's not 4");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.WriteLine("Sorry it's not 5");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Sorry it has to be a number between 1 and 5");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+             }
+            while (Guess == false);
+            Console.Read();
+        }
+    }
+}
